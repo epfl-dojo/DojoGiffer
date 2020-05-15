7420c0540d6703ae0f6d7b7bed35999eff332f05
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2020-05-15 15:34:55 +0200

    add working version using volley
    https://developer.android.com/training/volley/simple

--- app/src/main/java/com/example/dojo_giffer/ShowResult.kt
+++ app/src/main/java/com/example/dojo_giffer/ShowResult.kt
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.widget.TextView
-import okhttp3.*
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
 
 class ShowResult : AppCompatActivity() {
-
-    private val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,26 +20,49 @@ class ShowResult : AppCompatActivity() {
         setContentView(R.layout.activity_show_result)
         // Get the Intent that started this activity and extract the string
         val message = intent.getStringExtra(EXTRA_MESSAGE)
-        val result=run("https://jsonplaceholder.typicode.com/posts/1");
+//        val result=run("https://jsonplaceholder.typicode.com/posts/1");
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://jsonplaceholder.typicode.com/posts/1"
+
+        // Request a string response from the provided URL.
+
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                textView.text = "Response is: ${response}"
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
+
 
 
     }
 
+
     fun run(url: String) {
+        val text = URL("https://www.google.com").readText()
+        println(text)
 
-        val request = Request.Builder()
-            .url(url)
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {}
-            override fun onResponse(call: Call, response: Response) {
-                println(response.body()?.string())
-                // Capture the layout's TextView and set the string as its text
-                val textView = findViewById<TextView>(R.id.textView).apply {
-                    text = response.body()?.string()
-                }
-            }
-        })
+//        val request = Request.Builder()
+//            .url(url)
+//            .build()
+//
+//        client.newCall(request).enqueue(object : Callback {
+//            override fun onFailure(call: Call, e: IOException) {}
+//            override fun onResponse(call: Call, response: Response) {
+//                println(response.body()?.string())
+//                // Capture the layout's TextView and set the string as its text
+//                val textView = findViewById<TextView>(R.id.textView).apply {
+//                    text = response.body()?.string()
+//                }
+//            }
+//        })
     }
 }
