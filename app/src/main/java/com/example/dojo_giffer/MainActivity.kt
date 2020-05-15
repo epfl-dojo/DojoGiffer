547b8c9c2251c383b12978873a8e80a34f1c3002
Loïc Humbert <loichumbert5450@gmail.com>
2020-05-15 13:33:48 +0200

    Clicking OK goes to ShowResult activity

--- app/src/main/java/com/example/dojo_giffer/MainActivity.kt
+++ app/src/main/java/com/example/dojo_giffer/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.dojo_giffer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun searchGif(view: View) {
+        val editText = findViewById<EditText>(R.id.input_search)
+        val message = editText.text.toString()
+        val intent = Intent(this, ShowResult::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/dojo_giffer/ShowResult.kt
@@ -0,0 +1,12 @@
+package com.example.dojo_giffer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowResult : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_result)
+    }
+}
