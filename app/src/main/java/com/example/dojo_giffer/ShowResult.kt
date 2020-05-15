b3bdf414d76cbd35c32b371b8f9e13fa6450e66c
Christian Zufferey <christian@zufferey.com>
2020-05-15 13:47:34 +0200

    wip

--- app/src/main/java/com/example/dojo_giffer/ShowResult.kt
+++ app/src/main/java/com/example/dojo_giffer/ShowResult.kt
@@ -2,11 +2,21 @@ package com.example.dojo_giffer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.TextView
 
 class ShowResult : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_result)
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture the layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+
     }
 }
